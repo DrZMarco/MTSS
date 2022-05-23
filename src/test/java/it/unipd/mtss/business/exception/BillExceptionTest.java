7213b8077da1a1e6e1b21213e9f7964b2eeee1ba
trincaw <nicoloxtrinca@gmail.com>
2022-05-23 17:30:46 +0200

    Close #2; Create classi base

--- src/test/java/it/unipd/mtss/business/exception/BillExceptionTest.java
+++ src/test/java/it/unipd/mtss/business/exception/BillExceptionTest.java
@@ -3,3 +3,18 @@
 // [NICOLO] [TRINCA] [2011070]
 ////////////////////////////////////////////////////////////////////
 
+package it.unipd.mtss.business.exception;
+import org.junit.Test;
+
+
+public class BillExceptionTest {
+    @Test(expected = BillException.class)
+    public void constructor_emptyConstructor_ExceptionThrownWithEmptyConstructor() throws BillException {
+        throw new BillException();
+    }
+
+    @Test(expected = BillException.class)
+    public void constructor_messageConstructor_ExceptionThrownWithMessageConstructor() throws BillException {
+        throw new BillException("Thrown new exception");
+    }
+}
